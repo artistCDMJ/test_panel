32a19e9ef909ecac02ec9de12dab9c3cc119e20e
Craig Jones <artistcdmj@gmail.com>
2016-07-26 22:29:11 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,11 +37,20 @@ class SolidfyDifference(bpy.types.Operator):
         
         for obj in sel:
             context.scene.objects.active = obj#set active to selected
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.dissolve_faces()
+            bpy.ops.object.editmode_toggle()
+
             bpy.ops.object.modifier_add(type='SOLIDIFY')#set soldifiy for bool
             bpy.context.object.modifiers["Solidify"].thickness = 0.3#thicker than active
             bpy.ops.transform.translate(value=(0, 0, 0.01), constraint_axis=(False, False, True), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1, release_confirm=True)#attempt to only move bool brush up in Z
             
             context.scene.objects.active = act#reset active 
+            
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.dissolve_faces()
+            bpy.ops.object.editmode_toggle()
+
             
             bpy.ops.object.modifier_add(type='SOLIDIFY')#basic soldify for boolean
             bpy.ops.transform.translate(value=(0, 0, 0), constraint_axis=(False, False, True), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1, release_confirm=True)#to move active 0 in Z
@@ -73,11 +82,20 @@ class SolidfyUnion(bpy.types.Operator):
         
         for obj in sel:
             context.scene.objects.active = obj#set active to selected
+            
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.dissolve_faces()
+            bpy.ops.object.editmode_toggle()
+            
             bpy.ops.object.modifier_add(type='SOLIDIFY')#set soldifiy for bool
             bpy.context.object.modifiers["Solidify"].thickness = 0.3#thicker than active
             #bpy.ops.transform.translate(value=(0, 0, 0.01), constraint_axis=(False, False, True), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1, release_confirm=True)#attempt to only move bool brush up in Z
             
             context.scene.objects.active = act#reset active 
+            
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.dissolve_faces()
+            bpy.ops.object.editmode_toggle()
             
             bpy.ops.object.modifier_add(type='SOLIDIFY')#basic soldify for boolean
             bpy.context.object.modifiers["Solidify"].thickness = 0.3#thicker than active
@@ -92,7 +110,46 @@ class SolidfyUnion(bpy.types.Operator):
         
         #user moves parent and then adjusts child
         #user calls reproject while in obj mode which returns mask to texpaint
+        
+        
+#next operator
+class RemoveMods(bpy.types.Operator):
+    """Remove Modifiers"""
+    bl_idname = "object.remove_modifiers"
+    bl_label = "Remove Modifiers"
+    bl_options = { 'REGISTER','UNDO' }
+    
+    def execute(self, context):
+        scene = context.scene
+                
+        
+        #new code
+        context = bpy.context
+        scene = context.scene
+        obj = context.object
 
+        # get a reference to the current obj.data
+        old_mesh = obj.data
+
+        # settings for to_mesh
+        apply_modifiers = False
+        settings = 'PREVIEW'
+        new_mesh = obj.to_mesh(scene, apply_modifiers, settings)
+
+        # object will still have modifiers, remove them
+        obj.modifiers.clear()
+
+        # assign the new mesh to obj.data 
+        obj.data = new_mesh
+
+        # remove the old mesh from the .blend
+        bpy.data.meshes.remove(old_mesh)
+        bpy.context.object.draw_type = 'SOLID'
+
+        
+                
+            
+        return {'FINISHED'}
 
 
 
@@ -115,12 +172,13 @@ class TestPanel(bpy.types.Panel):
         row.label(text="Test Panel")
         
         row = layout.row()
-        row.operator("object.reproject_mask", text = "Reproject Mask", icon = 'ERROR')
+        row.operator("object.reproject_mask", text = "Reproject Mask", icon = 'NODE_SEL')
         row = layout.row()
-        row.operator("object.solidfy_difference", text = "Solidify Diff", icon = 'PLUGIN')
+        row.operator("object.solidfy_difference", text = "Solidify Diff", icon = 'ROTACTIVE')
         row = layout.row()
-        row.operator("object.solidfy_union", text = "Solidify Union", icon = 'PLUGIN')
-            
+        row.operator("object.solidfy_union", text = "Solidify Union", icon = 'ROTATECOLLECTION')
+        row = layout.row()
+        row.operator("object.remove_modifiers", text = "Remove Modifiers", icon = 'RECOVER_LAST')    
 
 
 
@@ -130,15 +188,18 @@ def register():
     bpy.utils.register_class(ReprojectMask)
     bpy.utils.register_class(SolidfyDifference)
     bpy.utils.register_class(SolidfyUnion)
+    bpy.utils.register_class(RemoveMods)
     bpy.utils.register_class(TestPanel)
     
 def unregister():
     bpy.utils.unregister_class(ReprojectMask)
     bpy.utils.unregister_class(SolidfyDifference)
     bpy.utils.unregister_class(SolidfyUnion)
+    bpy.utils.unregister_class(RemoveMods)
     bpy.utils.unregister_class(TestPanel)
     
     
        
 if __name__ == "__main__":
     register()
+
